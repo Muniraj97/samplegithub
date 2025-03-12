77c5c98648649ccaae1208fa835c24742e78ac23
Muniraj97 <muniram14meiet92@gmail.com>
2025-03-12 17:50:17 +0530

    hiii

---
+++ src/org/demo/CompanyName.java
@@ -0,0 +1,14 @@
+package org.demo;
+
+import java.io.File;
+
+public class CompanyName {
+	public static void main(String[] args) {
+		File f=new File("D:\\Selenium Class//kumar//muni//david");
+		if(!f.exists()) {
+			f.mkdirs();	
+		}
+		
+	}
+}
+
